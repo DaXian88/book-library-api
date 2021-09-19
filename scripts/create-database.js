e12de3b7f7b31f0f5124a293240745d743e8a3e3
Khalid Safy <khalid_safy88@hotmail.com>
2021-09-19 21:25:25 +0100

    Added test setup

--- scripts/create-database.js
+++ scripts/create-database.js
@@ -24,7 +24,7 @@ const setUpDatabase = async () => {
   try {
 
     // connect to the database
-    const connection = await mysql.createConnection({
+    const db = await mysql.createConnection({
       host: DB_HOST,
       user: DB_USER,
       password: DB_PASSWORD,
