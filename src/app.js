857bb121a17127302dc0bae1ac5d813db26e8651
Khalid Safy <khalid_safy88@hotmail.com>
2021-09-21 23:40:50 +0100

    Added controller for POST reader and passing initial test

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const readerController = require('./controllers/reader')
+const { readerModel } = require('./models/reader')
+
 const app = express();
 
-app.use(express.json);
+app.use(express.json());
+
+app.post('/readers', readerController.create)
 
 
 module.exports = app;
